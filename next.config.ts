2a79a7221ab509a1a77cbb3663a1cb179f5014a7
GARUDA393 <swami.anu30@gmail.com>
2026-09-06 23:55:39 +0530

    Added new config

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  output: "export",
+
+  basePath: isProduction ? "/Portfolio" : "",
+
+  images: {
+    unoptimized: true
+  },
+
+  trailingSlash: true
+};
+
+export default nextConfig;
